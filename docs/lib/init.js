a78406acbe2154dcc2966aae996bf576aec44946
caalma <nds.suscript@gmail.com>
2025-12-02 18:28:44 -0300

    Ajustes web

--- docs/lib/init.js
+++ docs/lib/init.js
@@ -1,3 +1,27 @@
-// Por ahora vacío – queda listo para cuando quieras meter visualizadores,
-// cambio automático de título, etc.
-console.log("ganuritov ∿ online");
+var hydra = new Hydra({
+    canvas: document.getElementById("hydraCanvas"),
+    detectAudio: false,
+    width: window.innerWidth,
+    height: window.innerHeight
+});
+
+
+const Anim = [
+    () => {
+        osc(10, 0.04, Math.random()*5)
+            .rotate(3)
+            .brightness(-0.6)
+            .out();
+    },
+    () => {
+        noise(1)
+            .colorama(Math.random()*5)
+            .brightness(-0.8)
+            .out()
+    }
+];
+
+window.addEventListener('load', () => {
+    setResolution(10,10);
+    Anim[Math.round(Math.random() * Anim.length)]();
+});
